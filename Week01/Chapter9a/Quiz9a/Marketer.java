db2ace12ab9d4c6e33b18011fd02b4dccc34cb09
Camden Harris <camdenharris@gmail.com>
2024-01-05 00:14:47 -0800

    Completed chapter 9a Quiz

---
+++ Week01/Chapter9a/Quiz9a/Marketer.java
@@ -0,0 +1,27 @@
+package Quiz9a;
+
+/*
+ * Camden Harris
+ * Winter 2024, C211, William Iverson
+ * 1/5/24
+ * Chapter 9a Quiz
+ * Data structure class for a Marketer
+ */
+
+public class Marketer extends Employee {
+    
+    // default constructor
+    public Marketer() {
+        super();
+    }
+
+    // Marketers get paid 10k more than employees
+    public double getSalary() {
+        return super.getSalary() + 10000;
+    }
+
+    // Marketer catchphrase
+    public String toString() {
+        return "Act now, while supplies last!";
+    }
+}
---
+++ Week01/Chapter9a/Quiz9a/Tester.java
@@ -0,0 +1,10 @@
+package Quiz9a;
+
+public class Tester {
+// copyright Bellevue College
+   public static void main(String[] args994) {
+      Employee bob = new Marketer();
+      System.out.println(bob); // Act now, while supplies last!
+      System.out.println(bob.getSalary()); // add extra 10,000
+   }
+}
