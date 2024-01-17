1006c08463af40c5a210c703df6c05418237ec79
Camden Harris <camdenharris@gmail.com>
2024-01-16 23:20:30 -0800

    Completed chapter 10 quiz

--- Week02/Assignment/Exercises10.java
+++ Week02/Assignment/Exercises10.java
@@ -12,8 +12,54 @@ import java.util.ArrayList;
 
 public class Exercises10 {
     
-    public static void main(String[] args) {
+    public static void main(String[] alot587) {
+	    
+        // Initialize an array of Strings, then load into a String List:
+        String[] arrayS = {"four", "score", "and", "seven", "years", "ago"};
+        ArrayList<String> testListS = new ArrayList<String>(); 
+        ArrayList<String> testListSempty = new ArrayList<String>();
+        for (String s: arrayS) testListS.add(s);
         
+        // Use Iverson's CalendarDate, so we MUST use compareTo concept
+        ArrayList<CalendarDate> testListC = new ArrayList<CalendarDate>();
+        ArrayList<CalendarDate> testListCempty = new ArrayList<CalendarDate>();
+        testListC.add(new CalendarDate(10,12,1314));  testListC.add(new CalendarDate(10,12,1314));
+        testListC.add(new CalendarDate(2,14,2022));  testListC.add(new CalendarDate(3,21,2022));
+         
+        // Exercise 10.2
+        System.out.println(testListS);
+        swapPairs(testListS);
+        swapPairs(testListSempty);
+        System.out.println("10.2 swapPairs: "+ testListS); 
+        System.out.println();
+        
+        // Exercise 10.6
+        System.out.println(testListC);
+        minToFront(testListC);
+        minToFront(testListCempty);
+        System.out.println("10.6 minToFront: " + testListC);
+        System.out.println();
+        
+        // Exercise 10.7
+        System.out.println(testListC);
+        removeDuplicates(testListC);
+        removeDuplicates(testListCempty);
+        System.out.println("10.7 removeDuplicates: " + testListC);
+        System.out.println();
+        
+        // Exercise 10.12
+        System.out.println(testListS);
+        markLength4(testListS);
+        markLength4(testListSempty);
+        System.out.println("10.12 markLength4: " + testListS);
+        System.out.println();
+    
+        // Exercise 10.18
+        System.out.println(testListS);
+        mirror(testListS);
+        mirror(testListSempty);
+        System.out.println("10.18 mirror: " + testListS);
+        System.out.println();
     }
 
     // Exercise 10.2 largestAbsVal
@@ -44,7 +90,7 @@ public class Exercises10 {
         CalendarDate currentDate;
         for (int i = 1; i < list.size(); i++) {
             currentDate = list.get(i);
-            if( currentDate.compareTo(minDate) < 0 ) {
+            if( minDate.compareTo(currentDate) < 0 ) {
                 minDate = currentDate;
                 minIndex = i;
             }
